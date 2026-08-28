ffda1d96f5967e7827dd477a024b37222f68996b
Syavira Hervianita <rifkisenot@gmail.com>
2026-08-28 11:47:58 +0700

    Fix GitHub Pages root deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/pendaftaran-online-pasien",
-  assetPrefix: "/pendaftaran-online-pasien/",
   trailingSlash: true,
 
   typescript: {
